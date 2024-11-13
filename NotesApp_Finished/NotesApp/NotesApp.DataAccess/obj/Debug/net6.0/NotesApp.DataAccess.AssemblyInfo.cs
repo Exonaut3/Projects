3778d1b2e55eb8df25d5b93dbbfa5abc406ab4b0
Simeon Dvojakov <dvojakovsimeon@gmail.com>
2024-11-13 12:38:36 +0100

    Projects

--- NotesApp_Finished/NotesApp/NotesApp.DataAccess/obj/Debug/net6.0/NotesApp.DataAccess.AssemblyInfo.cs
+++ NotesApp_Finished/NotesApp/NotesApp.DataAccess/obj/Debug/net6.0/NotesApp.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NotesApp.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b30a0fb44cafdde8509ed31bb4013069b00b6f13")]
 [assembly: System.Reflection.AssemblyProductAttribute("NotesApp.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NotesApp.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
